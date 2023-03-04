c06d164646e4f66b617fa168674c96727561f6a1
Mario Tobar <104882791+mario-th@users.noreply.github.com>
2023-03-03 22:04:22 -0600

    Add files via upload

---
+++ Desafio1/Desafio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Cual es tu nombre");
+        string nombre = Console.ReadLine();
+
+        Console.WriteLine("Por favor escriba sus ingresos de los ultimos 3 meses");
+
+        Console.Write("mes 1: ");
+        double mes1 = double.Parse(Console.ReadLine());
+
+        Console.Write("mes 2: ");
+        double mes2 = double.Parse(Console.ReadLine());
+
+        Console.Write("mes 3: ");
+        double mes3 = double.Parse(Console.ReadLine());
+
+        double suma = mes1 + mes2 + mes3;
+        double promedio = suma / 3;
+
+        Console.WriteLine($"Hola {nombre} en total ganaste Q.{suma} y el promedio es Q.{promedio}");
+    }
+}
---
+++ OperacionesBasicas/OperacionesBasicas/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class OperacionesBasicas
+{
+    public static int Sumar(int a, int b)
+    {
+        return a + b;
+    }
+    public static int Restar(int a, int b)
+    { 
+        return a - b; 
+    }
+
+    public static int Multiplicar(int a, int b)
+    {
+        return a * b;
+    }
+
+    public static int Dividir(int a, int b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("No se puede dividir entre cero");
+        }
+        return a / b;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("***** Programa para calcular operaciones basicas *****");
+        Console.Write("ingrese el primer numero entero: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("ingrese el segundo numero entero: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int suma = OperacionesBasicas.Sumar(a, b);
+        int resta = OperacionesBasicas.Restar(a, b);
+        int multiplicacion = OperacionesBasicas.Multiplicar(a, b);
+        int division = OperacionesBasicas.Dividir(a, b);
+
+        Console.WriteLine($"La suma de {a} y {b} es: {suma}");
+        Console.WriteLine($"La resta de {a} y {b} es: {resta}");
+        Console.WriteLine($"La multiplicacion de {a} y {b} es: {multiplicacion}");
+        Console.WriteLine($"La division de {a} y {b} es: {division}");
+    }
+
+}
+
